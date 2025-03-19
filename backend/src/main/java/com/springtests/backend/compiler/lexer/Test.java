a7ea5f90f56f519c78e412fd2f8dbc31a5e4abe5
Smorareq1 <smorareq1@gmail.com>
2025-03-19 14:02:23 -0600

    Added the regular expressions for booleanos and caracter, fixed the real numbers

--- backend/src/main/java/com/springtests/backend/compiler/lexer/Test.java
+++ backend/src/main/java/com/springtests/backend/compiler/lexer/Test.java
@@ -6,12 +6,13 @@ public class Test {
     public static void main(String[] args) {
         String code = """
                 // Este es un comentario de una línea
-                Entero contador. = 0;  // Inicialización del contador
+                Entero contador = 3.15;  // Inicialización del contador
                 /* Este es un comentario
                    multilínea */
                 if (@contador == 0) {
                     contador = 1;
                 }
+                'a'
                 escribir("El valor del contador es: ", contador);
                 """;
         Lexer lexer = new Lexer(); 
